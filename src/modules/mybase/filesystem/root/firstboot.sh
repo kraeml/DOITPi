f008d33627a754b87b403cc50dfe403baae12031
kraeml <kraeml@sesamestreet.franken.de>
2022-10-15 10:43:25 +0200

    Datei README in Orndern filesystem entfernt

--- src/modules/mybase/filesystem/root/firstboot.sh
+++ src/modules/mybase/filesystem/root/firstboot.sh
@@ -5,11 +5,12 @@
 function rm_home_pi {
   if grep -r '/home/pi' $1
   then
-	  grep -rl '/home/pi' $1 | xargs sed -i 's@/home/pi@/home/'$(getent passwd 1000 | cut --delimiter=: --fields=1)'@g'
+	  grep -rl '/home/pi' $1 | xargs sed -i 's@/home/pi@/'$(getent passwd 1000 | cut --delimiter=: --fields=6)'@g'
   fi
 }
 
 rm_home_pi /etc/systemd/system
+rm_home_pi $(getent passwd 1000 | cut --delimiter=: --fields=6)
 
 systemctl disable firstboot.service
 
