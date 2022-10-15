16aa21760b3c811c8834ed940d1d244042f33128
kraeml <kraeml@sesamestreet.franken.de>
2022-10-15 21:04:04 +0200

    Das Slash am Anfang von Homepfad wurde entfernt. Ansible update Paket cache

--- src/modules/mybase/filesystem/root/firstboot.sh
+++ src/modules/mybase/filesystem/root/firstboot.sh
@@ -5,12 +5,17 @@
 function rm_home_pi {
   if grep -r '/home/pi' $1
   then
-	  grep -rl '/home/pi' $1 | xargs sed -i 's@/home/pi@/'$(getent passwd 1000 | cut --delimiter=: --fields=6)'@g'
+	  grep -rl '/home/pi' $1 | xargs sed -i 's@/home/pi@'$(getent passwd 1000 | cut --delimiter=: --fields=6)'@g'
   fi
 }
 
 rm_home_pi /etc/systemd/system
 rm_home_pi $(getent passwd 1000 | cut --delimiter=: --fields=6)
+
+ansible --extra-vars ansible_python_interpreter=/usr/bin/python3 \
+  --inventory localhost, --connection local \
+  --module-name apt \
+  --args "update_cache=yes cache_valid_time=3600" localhost
 
 systemctl disable firstboot.service
 
